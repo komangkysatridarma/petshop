28f8560617022e08eb5d120333e1a550604af0b1
komangkysatridarma <komangkysatridarma@smkwikrama.sch.id>
2025-11-18 20:56:45 +0700

    feat: function update and delete for branchController

--- controller/branch_controller.go
+++ controller/branch_controller.go
@@ -81,5 +81,74 @@ func (controller *BranchController) Create(ctx *gin.Context) {
 
 func (controller *BranchController) Update(ctx *gin.Context){
 	req := request.UpdateBranchRequest{}
-	
+	err := ctx.ShouldBindBodyWithJSON(&req)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, response.ErrorResponse{
+			Code: 400,
+			Message: err.Error(),
+		})
+	}
+
+	branchId := ctx.Param("id")
+	id, err := strconv.Atoi(branchId)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, response.ErrorResponse{
+			Code: 400,
+			Message: "Invalid Id",
+		})
+		return
+	}
+
+	req.Id = id
+
+	err = controller.branchService.Update(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response.ErrorResponse{
+			Code: 500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	res := response.Response{
+		Code: 200,
+		Status: "Ok",
+		Data: req,
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (controller *BranchController) Delete(ctx *gin.Context){
+	branchId := ctx.Param("id")
+	id, err := strconv.Atoi(branchId)
+
+	_, err = controller.branchService.FindById(id)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, response.ErrorResponse{
+			Code: 404,
+			Message: "Id Not Found",
+		})
+		return
+	}
+
+	err = controller.branchService.Delete(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response.ErrorResponse{
+			Code: 500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	res := response.Response{
+		Code: 200,
+		Status: "Ok",
+		Data: nil,
+	}
+
+	ctx.JSON(http.StatusOK, res)
 }
